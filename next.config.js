67a43b364e2e94edc4df46802a6ca8b3b60d3028
TomScott <contact@tascott.co.uk>
2024-08-08 15:50:44 +0100

    Remove env

--- next.config.js
+++ next.config.js
@@ -1,23 +1,33 @@
-if (!URL.canParse(process.env.WORDPRESS_API_URL)) {
+if(!process.env.WORDPRESS_API_URL) {
   throw new Error(`
     Please provide a valid WordPress instance URL.
     Add to your environment variables WORDPRESS_API_URL.
   `);
 }
 
-const { protocol, hostname, port, pathname } = new URL(
-  process.env.WORDPRESS_API_URL,
-);
+const wordPressUrl = process.env.WORDPRESS_API_URL.startsWith('http')
+  ? process.env.WORDPRESS_API_URL
+  : `http://${process.env.WORDPRESS_API_URL}`;
+
+const {protocol,hostname,port} = new URL(wordPressUrl);
 
 /** @type {import('next').NextConfig} */
 module.exports = {
   images: {
     remotePatterns: [
       {
-        protocol: protocol.slice(0, -1),
+        protocol: protocol.slice(0,-1),
         hostname,
         port,
-        pathname: `${pathname}/**`,
+        pathname: '/**',
+      },
+      {
+        protocol: 'http',
+        hostname: '*.gravatar.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.gravatar.com',
       },
     ],
   },
